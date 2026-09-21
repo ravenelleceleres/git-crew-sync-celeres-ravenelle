d75c6b3284cb95f15e91192001be29f05f05df7b
HUAWEI <ravefcng@gmail.com>
2026-09-21 20:58:44 +0800

    Add overtime pay for shifts over 8 hours - celeres.ravenelle

--- shifts.js
+++ shifts.js
@@ -3,6 +3,11 @@ function isValidShift(hours) {
 }
 
 function calculatePay(hours, rate) {
+  if (hours > 8) {
+    let regularPay = 8 * rate;
+    let overtimePay = (hours - 8) * rate * 1.5;
+    return Math.floor(regularPay + overtimePay);
+  }
   return Math.floor(hours * rate);
 }
 
